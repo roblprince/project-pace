17e06b1bd9580fd34d5404c7377e70dc3bc3b999
roblprince <32687474+roblprince@users.noreply.github.com>
2017-10-10 17:06:55 -0400

    SeekerStateMachine.cs
    Primary enemy type | behavior: seeks player within pursuit range and lunges in straight dashes

---
+++ SeekerStateMachine.cs
@@ -0,0 +1,53 @@
+//Inherits EnemyStateMachine.cs
+
+using UnityEngine;
+using System.Collections;
+
+public class SeekerStateMachine : EnemyStateMachine {
+
+	public override void Start(){
+		base.Start();
+
+		pursueMinRange = lungeDetectionRange;
+
+		SetState( States.IDLESTATE );
+	}
+	
+	void FixedUpdate() {
+		UpdateLoop();
+	}
+
+	protected override void IdleState() {
+		DeathCheck();
+		CountCooldown();
+
+		if( DetectPlayerByLOS() ) { SetState( States.PURSUESTATE ); }
+	}
+
+	protected override void PursueState() {
+		DeathCheck();
+		PursuePlayer();
+		CountCooldown();
+
+        if ( DetectPlayerByRange( lungeDetectionRange ) &&
+		   LungeIsOnCooldown() == false ) {
+
+			SetState( States.PREATTACKSTATE );
+		}
+	}
+
+	protected override void LungeState() {
+		DeathCheck();
+		Lunge();
+        if ( IsInAttackState == false ) { IsInAttackState = true; }
+		EndLungeState();
+	}
+
+	protected override void PreAttackState() {
+		PreAttackFlicker();
+		DeathCheck();
+		PursuePlayer();
+		
+		if( TimeSinceLastStateChange > preAttackDuration ) { SetState( States.LUNGESTATE ); }
+	}
+}
